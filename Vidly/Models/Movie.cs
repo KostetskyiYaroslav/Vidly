0b4d1b4d0313c0539ba68386ab3a4e573f90e865
Yaroslav <ya.kosteckiy@gmail.com>
2016-12-12 23:26:24 +0200

    created class for entities

---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.Append("Id = " + this.Id);
+            sb.Append("\nName = " + this.Name);
+            return sb.ToString();
+        }
+    }
+}
