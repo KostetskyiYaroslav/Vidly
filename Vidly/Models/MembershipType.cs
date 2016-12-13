a2e941ee11021836f4d9a15d19e0b4fdcba92aaf
Yaroslav <ya.kosteckiy@gmail.com>
2016-12-13 02:08:06 +0200

    added  field to model

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,9 @@ namespace Vidly.Models
     public class MembershipType
     {
         public byte Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
